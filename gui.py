4611b58c034fe0f0fac807f0d5c93e6c72b138db
Nicolas.C <comcomnico@gmail.com>
2021-04-19 19:36:09 +0200

    changed current gui to legacy and test to current + start to bind buttons

--- gui.py
+++ gui.py
@@ -1,63 +1,3 @@
-# from kivy.app import App
-# from kivy.uix.gridlayout import GridLayout
-# from kivy.uix.label import Label
-# from kivy.uix.textinput import TextInput
-# from kivy.uix.togglebutton import ToggleButton
-# from kivy.uix.button import Button
-# from kivy.uix.pagelayout import PageLayout
-# from kivy.uix.boxlayout import BoxLayout
-# from kivy.uix.widget import Widget
-# from kivy.uix.screenmanager import ScreenManager, Screen
-
-# class LoginScreen(GridLayout):
-
-#     def __init__(self, **kwargs):
-#         super(LoginScreen, self).__init__(**kwargs)
-#         self.cols = 2
-#         btn1 = ToggleButton(text='Male', group='sex',)
-#         btn2 = ToggleButton(text='Female', group='sex', state='down')
-#         btn3 = ToggleButton(text='Mixed', group='sex')
-#         self.add_widget(btn1)
-#         self.add_widget(btn2)
-#         self.add_widget(btn3)
-
-# class Page_Layout(PageLayout):
-#     def __init__(self, **kwargs):
-#         super().__init__(**kwargs)
-
-#         self.layout1 = LoginScreen()
-
-#         self.add_widget(self.layout1)
-
-#         self.button2 = Button(
-#             text="Page 2"
-#         )
-
-#         self.add_widget(self.button2)
-
-#         self.button3 = Button(
-#             text="Page 3"
-#         )
-
-#         self.add_widget(self.button3)
-
-# class MyApp(App):
-#     def build(self):
-#         sm
-#         return Page_Layout()
-
-
-# if __name__ == '__main__':
-#     MyApp().run()
-
-
-
-
-
-
-
-
-
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
@@ -65,80 +5,30 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from CustomModules import CustomGraphics
 from kivy.uix.screenmanager import SlideTransition
+from kivy.uix.boxlayout import BoxLayout
+from kivy.config import Config
 
-# Builder.load_string("""
-# <BuildingScreen>:
-#     name: '_building_'
 
-#     GridLayout:
-#         cols: 1
-#         padding: 5,5,5,5
-#         Label:
-#             id: title
-#             text: "Select a building"
-#             width: 40
-#             size_hint_y: None
-#             color: 0,0,0,1
-#         GridLayout:
-#             cols: 2
-#             Label:
-#                 id: building
-#                 text: "Hi I'm The Building Screen"
-#             Label:
-#                 id: building
-#                 text: "Hi I'm The Building Screen"
-#             Label:
-#                 id: building
-#                 text: "Hi I'm The Building Screen"
-#             Label:
-#                 id: building
-#                 text: "Hi I'm The Building Screen"
-# <ControlPanelScreen>:
-#     name: '_control_panel_'
-#     Label:
-#         id: control_panel
-#         text: "Hi I'm The Control panel Screen"
-# """)
-
+Config.set('graphics', 'width', '1280')
+Config.set('graphics', 'height', '720')
 Builder.load_file('./builder.kv')
-sm = ScreenManager()
 
-class BuildingScreen(Screen):
-    def switchToControlPanel(self, widget, app):
-        print(widget.name)
-        print(app)
-        sm.transition = SlideTransition(direction='left')
-        sm.current = '_control_panel_'
-    # def __init__(self, name, **kwargs):
-    #     super(BuildingScreen, self).__init__(**kwargs)
-    #     self.name = name
-    #     layout = GridLayout(cols=2)
-    #     layout.add_widget(Button(text='Hello 1'))
-    #     layout.add_widget(Button(text='World 1'))
-    #     layout.add_widget(Button(text='Hello 2'))
-    #     layout.add_widget(Button(text='World 2'))
+class MainScreen(BoxLayout):
+    bat_8a = 0
+    bat_8b = 0
+    bat_8c = 0
+    nb_person = 0
 
-    #     self.do_layout(layout)
-
-
-class ControlPanelScreen(Screen):
-    def backToBuildings(self):
-        print("go back you dumb fuck")
-        sm.transition = SlideTransition(direction='right')
-        sm.current = '_building_'
-
-class MyApp(App):
+    def check_card(self):
+        print("_________________________________")
+        print("bat 8A door : " + str(self.bat_8a))
+        print("bat 8B door : " + str(self.bat_8b))
+        print("bat 8C door : " + str(self.bat_8c))
+        print("nb person : " + str(self.nb_person))
+    
+class app(App):
     def build(self):
-        # Create the screen manager
-        sm.add_widget(BuildingScreen(name='_building_'))
-        sm.add_widget(ControlPanelScreen(name='_control_panel_'))
-        sm.current = '_building_'
-        print(sm)
-        print(sm.children)
-
-        CustomGraphics.SetBG(sm, bg_color=[0.9,0.9,0.9,1])
-
-        return sm
+        return MainScreen()
 
 if __name__ == '__main__':
-    MyApp().run()
+    app().run()
